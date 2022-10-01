4332b9321ccc1c9ce625a7c122a74b833c303a4d
Mauro Marques <mauro.filho@luggit.app>
2022-10-01 14:33:40 +0100

    Finished ex1

--- Aula2/2.1/Ex1/f.m
+++ Aula2/2.1/Ex1/f.m
@@ -4,8 +4,8 @@
 
 lancamentos = rand(n, N) > p;
 mat_peloMenos1Rapaz = sum(lancamentos) >= 1;
-mat_apenasMais1Rapaz = sum(lancamentos) == 2; 
+mat_peloMenosMais1Rapaz = sum(lancamentos) >= 2; 
 prob_peloMenos1Rapaz = sum(mat_peloMenos1Rapaz)/N;
-prob_apenasMais1Rapaz = sum(mat_apenasMais1Rapaz)/N;
+prob_peloMenosMais1Rapaz = sum(mat_peloMenosMais1Rapaz)/N;
 
-prob = prob_apenasMais1Rapaz / prob_peloMenos1Rapaz
+prob = prob_peloMenosMais1Rapaz / prob_peloMenos1Rapaz
